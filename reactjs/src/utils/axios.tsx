39fc76dc98d1d3faed1babec60f2d4e83c815a82
ergundenizbuyruyk <ergunndenizbuyruk@gmail.com>
2024-04-29 00:59:29 +0300

    fixed axios response interceptor

--- reactjs/src/utils/axios.tsx
+++ reactjs/src/utils/axios.tsx
@@ -2,17 +2,23 @@ import React, { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
 import { useAuth } from "./auth";
 import { useNavigate } from "react-router-dom";
-import { ErrorDto, HttpResponse } from "../services/base/base.dto";
-import axios from "axios";
+import axios, { AxiosRequestConfig } from "axios";
 import { ProgressSpinner } from "primereact/progressspinner";
 import { BlockUI } from "primereact/blockui";
 import authService from "../services/auth/auth.service";
-import { AccessTokenDto, RefreshTokenDto } from "../services/auth/auth.dto";
+import { RefreshTokenDto } from "../services/auth/auth.dto";
 import { useToast } from "./toast";
 
 let AxiosContext = React.createContext(null);
 export function useAxiosContext() {
   return React.useContext(AxiosContext);
+}
+
+// Define the structure of a retry queue item
+interface RetryQueueItem {
+  resolve: (value?: any) => void;
+  reject: (error?: any) => void;
+  config: AxiosRequestConfig;
 }
 
 export function AxiosProvider({ children }: { children: React.ReactNode }) {
@@ -23,6 +29,12 @@ export function AxiosProvider({ children }: { children: React.ReactNode }) {
   const [blocked, setBlocked] = useState<boolean>(false);
   const [isAxiosInterceptorAdded, setIsAxiosInterceptorAdded] =
     useState<boolean>(false);
+
+  // Create a list to hold the request queue
+  const refreshAndRetryQueue: RetryQueueItem[] = [];
+
+  // Flag to prevent multiple token refresh requests
+  let isRefreshing = false;
 
   if (!isAxiosInterceptorAdded) {
     axios.interceptors.request.use(
@@ -53,42 +65,71 @@ export function AxiosProvider({ children }: { children: React.ReactNode }) {
         }
         return response;
       },
-      (error) => {
+      async (error) => {
+        const originalRequest: AxiosRequestConfig = error.config;
         if (error.config.blockUI) {
           setBlocked(false);
         }
         if (error.response.status === 401) {
-          try {
-            var user = auth.getUser();
-            const data: RefreshTokenDto = {
-              token: user.refreshToken,
-            };
-            return authService
-              .CreateTokenByRefreshToken(data, {
-                blockUI: false,
-              })
-              .then((response) => {
-                if (response.result && response.result.data) {
-                  auth.setToken(response.result.data, true);
-                  error.config.headers["Authorization"] =
-                    "Bearer " + response.result.data.accessToken;
-                  return axios.request({
-                    ...error.config,
-                    cache: false,
-                    overrideError: true,
-                  });
-                } else {
-                  auth.removeUserFromStorage();
-                  navigate("/login", { replace: true });
-                  return Promise.reject(error);
+          if (!isRefreshing) {
+            isRefreshing = true;
+            try {
+              var user = auth.getUser();
+              const data: RefreshTokenDto = {
+                token: user.refreshToken,
+              };
+              const newToken = await authService.CreateTokenByRefreshToken(
+                data,
+                {
+                  blockUI: false,
                 }
-              });
-            // return Promise.reject(error);
-          } catch (error) {
-            auth.removeUserFromStorage();
-            navigate("/login", { replace: true });
-            return Promise.reject(error);
+              );
+
+              if (newToken.result && newToken.result.data) {
+                auth.setToken(newToken.result.data, true);
+                error.config.headers["Authorization"] =
+                  "Bearer " + newToken.result.data.accessToken;
+
+                // Retry all requests in the queue with the new token
+                refreshAndRetryQueue.forEach(({ config, resolve, reject }) => {
+                  if (newToken.result && config.headers) {
+                    // override the bearer token
+                    config.headers["Authorization"] =
+                      "Bearer " + newToken.result.data.accessToken;
+                    axios
+                      .request(config)
+                      .then((response) => resolve(response))
+                      .catch((err) => reject(err));
+                  }
+                });
+
+                // Clear the queue
+                refreshAndRetryQueue.length = 0;
+
+                // Retry the original request
+                return axios(originalRequest);
+              } else {
+                auth.removeUserFromStorage();
+                navigate("/login", { replace: true });
+                return Promise.reject(error);
+              }
+            } catch (error) {
+              auth.removeUserFromStorage();
+              navigate("/login", { replace: true });
+              return Promise.reject(error);
+            } finally {
+              isRefreshing = false;
+            }
           }
+
+          // Add the original request to the queue
+          return new Promise<void>((resolve, reject) => {
+            refreshAndRetryQueue.push({
+              config: originalRequest,
+              resolve,
+              reject,
+            });
+          });
         } else if (error.response.status === 403) {
           auth.removeUserFromStorage();
           navigate("/login", { replace: true });
